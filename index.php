81ca023ab11cfb9031305d4f070ceb8f6b4a06b3
SesRiya <crdgorospe@gmail.com>
2021-11-22 15:08:19 +1300

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- font awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+          integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous"/>
+    <!-- Custom CSS file -->
+    <link rel="stylesheet" href="stylesheet/style.css">
+</head>
+
+
+<body>
+<!-- start #header-->
+<header>
+    <div class="topbuttons">
+        <p class="tophead">Rhea Gorospe</p>
+        <div class="logbuttons">
+            <a href="login.php">Login</a>
+            <a href="#">Wishlist</a>
+        </div>
+    </div>
+    <!--Primary Navigation-->
+    <nav class="navbar">
+        <a href="" class="logo">
+            <img src="images/Pop-Up.png" alt="Pop-Up Logo"/></a>
+        <ul class="nav-links">
+            <li><a href="index.php">Pix</a></li>
+            <li><a href="second.php">Tricks</a></li>
+            <li><a href="">biz</a></li>
+            <li><a href="">Vids</a></li>
+            <button class="open-button" onclick="openForm()">Sign-in</button>
+            <!-- <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a> -->
+        </ul>
+        <!--Primary Navigation-->
+</header>
